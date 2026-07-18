0620997b4a9bc842c867f839245714898d2535a0
mrhaomp2001 <huynhnhuthao248@gmail.com>
2026-07-18 11:22:12 +0700

    initialize project 3

---
+++ Witch Lexicapple Framework/Assets/Singleton/Scripts/Singleton.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static T _instance;
+    public static T Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindAnyObjectByType<T>();
+            }
+            return _instance;
+        }
+    }
+
+    protected virtual void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this as T;
+        }
+        else if (_instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
